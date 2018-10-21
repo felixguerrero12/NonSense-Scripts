448d185ef0f8aff62e690ec353ea018662c05bd9
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2018-10-21 15:56:32 -0400

    Update phonetic_alphabet.py

--- phonetic_alphabet.py
+++ phonetic_alphabet.py
@@ -3,7 +3,7 @@ def otan(word):
       'F':'Foxtrot','G':'Golf', 'H':'Hotel' , 'I':'India', 'J':'Juliet', 'K':'Kilo', 'L':'Lima',
       'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo',
       'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'Xray',
-      'Y':'Yankee', 'Z':'Zulu', '0':'Zero', '1':'One', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eigh', '9':'Nine'}
+      'Y':'Yankee', 'Z':'Zulu', '0':'Zero', '1':'One', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eigh', '9':'Nine', ' ':' '}
   phon = [dictionary[w] for w in word]
   return ' '.join(phon)
 
